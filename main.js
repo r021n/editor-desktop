9b5101a9465c79a9cd680aebf0eda1a02854639f
r021n <rozinabdul25@gmail.com>
2025-11-14 14:18:38 +0700

    refactor: streamline window creation and remove unnecessary comments

--- main.js
+++ main.js
@@ -11,23 +11,18 @@ function createWindow() {
   });
 
   win.loadFile("index.html");
-
-  // Opsional: Anda bisa menghapus baris ini jika tidak ingin menu bar (File, Edit, etc.)
-  // win.setMenu(null);
+  win.setMenuBarVisibility(false);
+  win.setMenu(null);
 }
 
-// Metode ini akan dipanggil ketika Electron selesai inisialisasi
-// dan siap untuk membuat jendela browser.
 app.whenReady().then(createWindow);
 
-// Keluar dari aplikasi saat semua jendela ditutup (kecuali di macOS).
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// Di macOS, buat jendela baru jika tidak ada yang terbuka saat ikon dock diklik.
 app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
